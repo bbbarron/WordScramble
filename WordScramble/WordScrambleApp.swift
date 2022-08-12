b72b26f1fb4757956e1ec2da25d40f79f1c61382
Barry Barron <barrybbarron@gmail.com>
2022-08-12 14:12:04 -0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Barry Barron on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
